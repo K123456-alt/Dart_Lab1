f1d539c7cadc7fabe47646eef114efd1dcffbed2
K123456-alt <kornachevkirill2008@gmail.comgit config user.email kornachevkirill2008@gmail.com>
2026-09-14 00:23:25 +0300

    learn dart functions

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -34,8 +34,51 @@ void main() {
   print(ids.length);
   List<String> fruits2 = ['яблоко', 'банан', 'груша'];
   for (var fruit in fruits2) {
+    print(fruit);
+
+String greet(String name) => 'Привет, $name!';
+int square(int x) => x * x;
+double half(double x) => x/2;
+void describePet({required String name, String
+species = 'кот', int age = 0}){
+  print('$name - $species, возраст $age');
+  
+}
+  describePet(name: 'Барсик', age: 3);
+  describePet(name: 'Шарик', species: 'пёс');
+String repeat(String text, [int times =2]){
+  String result = '';
+  for (int i = 0; i< times; i++){
+    result += text;
+  }
+  return result;
+}
+print(repeat('ха'));
+print(repeat('ха', 3));
+List<int> numbers = [3, 1, 4, 1, 5, 9];
+numbers.sort((a,b) => b - a);
+print(numbers);
+List<String> names = ['Чуча', 'Ваняя', 'Кирилл'];
+List<String> upper = names.map((name) =>
+name.toUpperCase()).toList();
+print(upper);
+List<String> longNames = names.where
+((name) => name.length > 4).toList();
+print(longNames);
 
   }
 
+  print(greet('Артём'));
+  print(greet('Мария'));
+  
+}
+
+
+
+String greet(String name){
+  return 'Привет, $name! ';
+
+
 
 }
+
